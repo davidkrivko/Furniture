098f79a9062f23fcc6327e2e9e489e6621c4ab07
Davyd Kryvko <davidkrivko@gmail.com>
2023-01-22 18:56:27 +0100

    Add database
    Create models, use django user for the project and run migrations

---
+++ furniture/models.py
@@ -0,0 +1,52 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from furniture_service import settings
+
+
+class Furniture(models.Model):
+    model = models.CharField(max_length=64)
+    type = models.ForeignKey(
+        "Type",
+        on_delete=models.DO_NOTHING,
+        related_name="furnitures",
+    )
+    description = models.TextField()
+    price = models.DecimalField(max_digits=12, decimal_places=3)
+    comments = models.ForeignKey(
+        "Commentary",
+        on_delete=models.CASCADE,
+        related_name="furnitures",
+    )
+
+
+class Type(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Commentary(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.DO_NOTHING,
+        related_name="commentaries",
+    )
+    furniture = models.ForeignKey(
+        Furniture,
+        on_delete=models.CASCADE,
+        related_name="commentaries",
+    )
+    content = models.TextField()
+
+
+class Order(models.Model):
+    furniture = models.ManyToManyField(Furniture)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="orders",
+    )
+    created_at = models.DateTimeField(auto_now=True)
+
+
+class User(AbstractUser):
+    pass

6a8612944dd075c1a813114a9f543d4822404210
Davyd Kryvko <davidkrivko@gmail.com>
2023-01-25 12:22:07 +0100

    Change behavior of admin panel Create custom AdminModels for furniture and order with through table. Create custom Admin panel for users without username

--- furniture/admin.py
+++ furniture/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
+from furniture.models import (
+    Furniture,
+    Type,
+    Commentary,
+    Order,
+    OrderItem,
+)
+
+
+@admin.register(Furniture)
+class FurnitureAdmin(admin.ModelAdmin):
+    list_display = ["model", "type", "price"]
+    list_filter = ["type"]
+    search_fields = ["model"]
+
+
+class OrderItemAdmin(admin.TabularInline):
+    model = OrderItem
+    extra = 2
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderItemAdmin,)
+    list_display = ["user", "created_time"]
+    list_filter = ["user"]
+
+
+admin.site.register(Order, OrderAdmin)
+admin.site.register(Type)
+admin.site.register(Commentary)
